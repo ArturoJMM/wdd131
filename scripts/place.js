5e6a24d2bfa67251d87b5bef0b01d7e4e23b95cc
ArturoJMM <slipknot25256@gmail.com>
2025-01-24 21:24:14 -0500

    Completed the responsive page for mobile and wide screens

---
+++ scripts/place.js
@@ -0,0 +1,18 @@
+const year = document.querySelector("#currentyear");
+const full = document.querySelector("#lastModified");
+
+const today = new Date();
+
+year.innerHTML = `${today.getFullYear()}`;
+
+full.innerHTML = `Last modified: ${new Intl.DateTimeFormat("en-US",{dateStyle: "full"}).format(today)}`;
+
+
+function calculateWindChill(temp=10, windSpeed=5) {
+    let windChill = 13.12 + 0.6215 * temp - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temp * Math.pow(windSpeed, 0.16);
+
+    return windChill.toFixed(2);
+}
+
+const windChill = document.querySelector("#windChill");
+windChill.innerHTML = calculateWindChill(10, 5);

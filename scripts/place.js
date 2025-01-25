9531236a9ed862c2a33f756cdb87eb1be27bd404
ArturoJMM <slipknot25256@gmail.com>
2025-01-24 22:31:47 -0500

    fixed windChill format

--- scripts/place.js
+++ scripts/place.js
@@ -8,11 +8,20 @@ year.innerHTML = `${today.getFullYear()}`;
 full.innerHTML = `Last modified: ${new Intl.DateTimeFormat("en-US",{dateStyle: "full"}).format(today)}`;
 
 
-function calculateWindChill(temp=10, windSpeed=5) {
-    let windChill = 13.12 + 0.6215 * temp - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temp * Math.pow(windSpeed, 0.16);
+function calculateWindChill(temp=28, windSpeed=5) {
 
-    return windChill.toFixed(2);
+    if (temp <= 10 && windSpeed > 4.8) {
+        let windChill = 13.12 + 0.6215 * temp - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temp * Math.pow(windSpeed, 0.16);
+
+        return `${windChill.toFixed(2)} °C`;
+    }
+
+    else {
+        return "N/A"
+    }
+    
+    
 }
 
 const windChill = document.querySelector("#windChill");
-windChill.innerHTML = `${calculateWindChill(10, 5)} °C`;
+windChill.innerHTML = calculateWindChill();

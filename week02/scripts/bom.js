d3b52b9305546bc297e628a391d91f8fa27f2418
ArturoJMM <slipknot25256@gmail.com>
2025-02-07 13:16:00 -0500

    Activity Completed with all the functionality

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,13 +1,29 @@
-//Selecting elemements
+//Selecting elements
 const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
 const list = document.querySelector("#list");
-//Creating new elements
-const li = document.createElement("li");
-const deleteButton = createElement("button");
-//Populating Elements
-li.textContent = input.value;
-deleteButton.textContent = "❌";
 
-li.append(deleteButton);
-list.append(li);
+button.addEventListener("click", function(){
+    if (input.value.trim() !== "") {
+        const li = document.createElement("li");
+        list.append(li);
+        li.textContent = input.value;
+
+        const deleteButton = document.createElement("button");
+        li.append(deleteButton);
+        deleteButton.textContent = "❌";
+
+        deleteButton.addEventListener("click", function(){
+            list.removeChild(li);
+            input.focus();
+        });
+
+        input.value = "";
+        input.focus();
+        }
+    
+    else {
+        alert("Input cannot be blank!");
+        input.focus();
+    }   
+});
